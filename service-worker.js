b79a7160a9b7a10de57ed63ef50dcf1e07dda1a5
YceHub <134993954+YceHub@users.noreply.github.com>
2025-08-26 02:25:13 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "planner-cache-v1";
+const FILES_TO_CACHE = [
+  "planner.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install SW
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
